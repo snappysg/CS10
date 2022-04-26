36b4985fd67d706cccb0b0ae2a05b7aa7967ba60
snappysg <102550087+snappysg@users.noreply.github.com>
2022-04-26 15:11:45 -0600

    Add files via upload

---
+++ initials.java
@@ -0,0 +1,36 @@
+
+
+/*
+
+Program: initials.java          Date: April 26, 2022 
+
+
+Author: Melvin Ulch 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+	public class initials
+	{
+	
+    public static void main(String args[])
+    {
+       System.out.println(" |\\  /|   |   | \n | \\/ |   |   | \n |    | 0 |___| \n  ");
+       
+	}
+	// Melvin Auto-generated method stub
+	
+	
+
+}
+
+
+
+/* Screen Dump
+ 
+
+
+ 
+ */
