6ab56f4f1d48983cc9107b6735d3cc40760b9e0c
snappysg <102550087+snappysg@users.noreply.github.com>
2022-05-06 11:02:20 -0600

    Add files via upload

---
+++ bingo.java
@@ -0,0 +1,39 @@
+
+
+/*
+
+Program: Assignment 3          Date: 4/21/22
+
+
+Author: Melvin Ulch
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class bingo 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("B      I      N      G      O");
+		System.out.println("3      22     45     50     67");
+		System.out.println("13     27     42     47     70");
+		System.out.println("6      20     FREE   60     73");
+		System.out.println("11     17     37     57     66");
+		System.out.println("9      30     33     55     75");
+	}
+		// Melvin Auto-generated method stub
+		
+		
+
+}
+
+
+
+/* Screen Dump
+ 
+
+
+ 
+ */
