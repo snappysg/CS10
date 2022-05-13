c69d7bf6208db72fecf715e41bbe859d98e4bfc6
snappysg <102550087+snappysg@users.noreply.github.com>
2022-05-13 11:25:11 -0600

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,40 @@
+/*
+
+Program: Assignment5.java          Date: May 13, 2022
+
+
+Author: Melvin Ulch  
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+
+
+
+
+public class Assignment5 
+{
+ public static void main(String args[])
+    {
+        int Prob1 = 79+3*(4+82-68)-7+19;
+        System.out.println("79 + 3 *(4+ 82– 68) – 7 + 19= " + Prob1);
+        
+        int Prob2 = (179+21+10)/7+181;
+        System.out.println("(179 + 21 + 10) / 7 + 181= " + Prob2);
+        
+        int Prob3 = 10389*56*11+2246;
+        System.out.println("10389 * 56 * 11 + 2246= " + Prob3);
+    }
+
+}
+
+/* Screen Dump
+ 
+79 + 3 *(4+ 82– 68) – 7 + 19= 145
+(179 + 21 + 10) / 7 + 181= 211
+10389 * 56 * 11 + 2246= 6401870
+
+ 
+ */
